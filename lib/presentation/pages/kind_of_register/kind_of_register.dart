94b197c7eeb95764dbfd9d11e193c3903f1316b1
ahmed <ahmedabdoelhawari12345work@gmail.com>
2022-10-28 22:51:17 +0200

    clean the code

--- lib/presentation/pages/kind_of_register/kind_of_register.dart
+++ lib/presentation/pages/kind_of_register/kind_of_register.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:uber/config/routes/route_app.dart';
@@ -8,7 +8,7 @@ import 'package:uber/presentation/cubit/personal_info_cubit/personal_info_cubit_
 import 'package:uber/presentation/layouts/base_layout.dart';
 import 'package:uber/presentation/pages/complete_user_info.dart';
 import 'package:uber/presentation/pages/register/register_page.dart';
-import 'package:uber/presentation/widgets/common/custom_widgets/custom_circulars_progress.dart';
+import 'package:uber/presentation/common_widgets/custom_circulars_progress.dart';
 
 class GetPersonalInfo extends StatefulWidget {
   final String userId;
@@ -26,36 +26,43 @@ class _GetPersonalInfoState extends State<GetPersonalInfo> {
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<PersonalInfoCubitCubit, PersonalInfoCubitState>(
+    return BlocListener<PersonalInfoCubitCubit, PersonalInfoCubitState>(
         bloc: PersonalInfoCubitCubit.get(context)..getUserInfo(widget.userId),
-        buildWhen: (previous, current) => previous != current,
-        builder: (context, state) {
-          if (state is PersonalInfoLoaded) {
-            WidgetsBinding.instance.addPostFrameCallback((_) async {
-              if (state.personalInfo == null) {
-                pushToPage(context,
-                    page: CompleteUserInfoPage(
-                        userId: widget.userId, phoneNumber: widget.phoneNumber));
-              } else {
-                await _sharePrefs.setString("userId", widget.userId);
-                if(!mounted)return;
-                Navigator.of(context).pushAndRemoveUntil(
-                    CupertinoPageRoute(
-                        builder: (context) =>
-                            BaseLayout(personalInfo: state.personalInfo!)),
-                    (route) => false);
-              }
-            });
-          } else if (state is PersonalInfoFailed) {
-            ToastShow.toastStateError(context,state.error);
-            WidgetsBinding.instance.addPostFrameCallback((_) {
-              Navigator.of(context).pushAndRemoveUntil(
-                  CupertinoPageRoute(
-                      builder: (context) => const RegisterPage()),
-                  (route) => false);
-            });
-          }
-          return const ThineCircularProgress();
-        });
+        listenWhen: (previous, current) => previous != current,
+        listener: listener,
+        child: const Scaffold(body: ThineCircularProgress()));
+  }
+
+  Future<void> listener(context, state) async {
+    if (state is PersonalInfoLoaded) {
+      registerUserType(state);
+    } else if (state is PersonalInfoFailed) {
+      goBackToRegisterPage(state);
+    }
+  }
+
+  registerUserType(PersonalInfoLoaded state) async {
+    bool isUserExist = state.personalInfo != null;
+    if (isUserExist) {
+      login(state);
+    } else {
+      continueTakingUserInfo();
+    }
+  }
+
+  continueTakingUserInfo() {
+    Go(context).to(CompleteUserInfoPage(
+        userId: widget.userId, phoneNumber: widget.phoneNumber));
+  }
+
+  login(PersonalInfoLoaded state) async {
+    await _sharePrefs.setString("userId", widget.userId);
+    if (!mounted) return;
+    Go(context).toAndRemoveAll(BaseLayout(personalInfo: state.personalInfo!));
+  }
+
+  goBackToRegisterPage(PersonalInfoFailed state) {
+    ToastShow.reformatToast(context, state.error);
+    Go(context).toAndRemoveAll(const RegisterPage());
   }
 }
