771597a2b25534464f539629a8dbd1a283ba82b5
ahmed <ahmedabdoelhawari12345work@gmail.com>
2022-10-28 22:50:27 +0200

    create values manager

---
+++ lib/core/resources/values_manager.dart
@@ -0,0 +1,71 @@
+class AppMargin {
+  static const double m8 = 8.0;
+  static const double m12 = 12.0;
+  static const double m14 = 14.0;
+  static const double m16 = 16.0;
+  static const double m18 = 18.0;
+  static const double m20 = 20.0;
+}
+
+class AppPadding {
+  static const double p2 = 2.0;
+  static const double p3 = 3.0;
+  static const double p4 = 4.0;
+  static const double p5 = 5.0;
+  static const double p6 = 6.0;
+  static const double p7 = 7.0;
+  static const double p8 = 8.0;
+  static const double p9 = 9.0;
+
+  static const double p10 = 10.0;
+  static const double p11 = 11.0;
+  static const double p12 = 12.0;
+  static const double p13 = 13.0;
+  static const double p14 = 14.0;
+  static const double p15 = 15.0;
+  static const double p16 = 16.0;
+  static const double p17 = 17.0;
+  static const double p18 = 18.0;
+  static const double p19 = 19.0;
+  static const double p20 = 20.0;
+
+  static const double p30 = 30.0;
+  static const double p40 = 40.0;
+  static const double p50 = 50.0;
+  static const double p60 = 60.0;
+  static const double p70 = 70.0;
+  static const double p80 = 80.0;
+  static const double p90 = 90.0;
+
+  static const double p100 = 100.0;
+  static const double p200 = 200.0;
+}
+
+class AppSize {
+  static const double s0 = 0;
+  static const double s1 = 1;
+  static const double s1_5 = 1.5;
+  static const double s4 = 4.0;
+  static const double s8 = 8.0;
+  static const double si8 = 8;
+  static const double s12 = 12.0;
+  static const double s14 = 14.0;
+  static const double s16 = 16.0;
+  static const double s18 = 18.0;
+  static const double s20 = 20.0;
+  static const double s28 = 28.0;
+  static const double s40 = 40.0;
+  static const double s60 = 60.0;
+  static const double s65 = 65.0;
+  static const double s90 = 90.0;
+  static const double s100 = 100.0;
+  static const double s120 = 120.0;
+  static const double s130 = 130.0;
+  static const double s140 = 140.0;
+  static const double s180 = 180.0;
+  static const double s190 = 190.0;
+}
+
+class DurationConstant {
+  static const int d300 = 300;
+}
