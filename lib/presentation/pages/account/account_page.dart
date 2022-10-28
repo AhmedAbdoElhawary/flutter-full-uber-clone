217ea2362d6dcbe680de8aa3c18390c05b856301
ahmed <ahmedabdoelhawari12345work@gmail.com>
2022-10-28 16:29:28 +0200

    create AccountPage

---
+++ lib/presentation/pages/account/account_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class AccountPage extends StatefulWidget {
+  const AccountPage({Key? key}) : super(key: key);
+
+  @override
+  State<AccountPage> createState() => _AccountPageState();
+}
+
+class _AccountPageState extends State<AccountPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+
+          ],
+        ),
+      ),
+    );
+  }
+}
