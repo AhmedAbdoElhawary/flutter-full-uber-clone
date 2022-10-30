af26a28bfc5d3332c2e6040557a92c32dd390809
ahmed <ahmedabdoelhawari12345work@gmail.com>
2022-10-30 22:40:06 +0200

    replace with controller

--- lib/presentation/pages/register/register_page/view/register_page.dart
+++ lib/presentation/pages/register/register_page/view/register_page.dart
@@ -1,38 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
-import 'package:uber/config/routes/route_app.dart';
-import 'package:uber/core/functions/toast_show.dart';
 import 'package:uber/core/resources/color_manager.dart';
 import 'package:uber/core/resources/strings_manager.dart';
 import 'package:uber/core/resources/styles_manager.dart';
 import 'package:uber/presentation/common_widgets/custom_elevated_button.dart';
 import 'package:uber/presentation/common_widgets/text_field/mobile_phone_text_field.dart';
-import 'package:uber/presentation/cubit/firebaseAuthCubit/firebase_auth_cubit.dart';
-import 'package:uber/presentation/pages/register/code_verification.dart';
+import 'package:uber/presentation/pages/register/register_page/logic/register_page_logic.dart';
 
-class RegisterPage extends StatefulWidget {
+class RegisterPage extends StatelessWidget {
   const RegisterPage({super.key});
 
   @override
-  State<RegisterPage> createState() => _RegisterPageState();
-}
-
-class _RegisterPageState extends State<RegisterPage> {
-  TextEditingController controller = TextEditingController();
-  @override
   Widget build(BuildContext context) {
+    final RegisterPageController controller = Get.put(RegisterPageController());
+
     return Scaffold(
       body: SafeArea(
         child: SingleChildScrollView(
             keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
             physics: const NeverScrollableScrollPhysics(),
-            child: buildColumn()),
+            child: buildColumn(context, controller)),
       ),
     );
   }
 
-  Widget buildColumn() {
+  Widget buildColumn(BuildContext context, RegisterPageController controller) {
     return Padding(
       padding: REdgeInsets.symmetric(horizontal: 15, vertical: 20.0),
       child: Column(
@@ -43,26 +36,14 @@ class _RegisterPageState extends State<RegisterPage> {
           Padding(
             padding: REdgeInsets.symmetric(vertical: 10),
             child: MobilePhoneTextField(
-
-                controller: controller, hint: "0100 123 4567"),
+                controller: controller.phoneController.value,
+                hint: "0100 123 4567"),
           ),
-
           Padding(
             padding: REdgeInsets.symmetric(vertical: 10.0),
             child: CustomElevatedButton(
                 backgroundColor: ColorManager.black,
-                onPressed: () async {
-                  String mobilePhone = controller.text;
-                  if (mobilePhone.length < 11) {
-                    ToastShow.toast(context, "The mobile phone is not valid.");
-                  } else {
-                    await FirebaseAuthCubit.get(context)
-                        .submitPhoneNumber(mobilePhone);
-                    if (!mounted) return;
-                    Go(context)
-                        .to(CodeVerificationPage(mobilePhone: controller.text));
-                  }
-                },
+                onPressed: () async => controller.onPressedContinue(context),
                 child: Text(
                   "Continue",
                   style:
@@ -70,11 +51,11 @@ class _RegisterPageState extends State<RegisterPage> {
                 )),
           ),
           const OrText(),
-          ContinueWith(
+          _ContinueWith(
               text: "Google", icon: Icons.local_airport, onPressed: () {}),
-          ContinueWith(
+          _ContinueWith(
               text: "Apple", icon: Icons.apple_rounded, onPressed: () {}),
-          ContinueWith(
+          _ContinueWith(
               text: "Facebook", icon: Icons.facebook_rounded, onPressed: () {}),
           const RSizedBox(height: 30),
           ...someText(),
@@ -104,11 +85,11 @@ class _RegisterPageState extends State<RegisterPage> {
       ];
 }
 
-class ContinueWith extends StatelessWidget {
+class _ContinueWith extends StatelessWidget {
   final String text;
   final IconData icon;
   final VoidCallback onPressed;
-  const ContinueWith({
+  const _ContinueWith({
     Key? key,
     required this.text,
     required this.icon,
