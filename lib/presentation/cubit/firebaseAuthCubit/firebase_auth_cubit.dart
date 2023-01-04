7334a62734644712dea7078045554d856774f301
ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-01-04 23:09:09 +0200

    refactoring file without use cases

--- lib/presentation/cubit/firebaseAuthCubit/firebase_auth_cubit.dart
+++ lib/presentation/cubit/firebaseAuthCubit/firebase_auth_cubit.dart
@@ -1,44 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:uber/domain/use_cases/auth/log_out.dart';
-import 'package:uber/domain/use_cases/auth/submit_otp.dart';
-import 'package:uber/domain/use_cases/auth/submit_phone_umber.dart';
+import 'package:uber/domain/repositories/auth_repository.dart';
 part 'firebase_auth_state.dart';
 
 class FirebaseAuthCubit extends Cubit<FirebaseAuthCubitState> {
-  final VerifyMobilePhoneUseCase _submitPhoneNumUseCase;
-  final SubmitOtpPhoneUseCase _submitOtpPhoneUseCase;
-  final LogOutUseCase _logOutUseCase;
+  final FirebaseAuthRepository _firebaseAuthRepository;
   String? userUid;
 
-  FirebaseAuthCubit(this._submitPhoneNumUseCase, this._submitOtpPhoneUseCase,
-      this._logOutUseCase)
-      : super(CubitInitial());
+  FirebaseAuthCubit(this._firebaseAuthRepository) : super(CubitInitial());
 
   static FirebaseAuthCubit get(BuildContext context) =>
       BlocProvider.of(context);
 
   Future<void> submitPhoneNumber(String mobilePhone) async {
     emit(MobilePhoneVerifying());
-    await _submitPhoneNumUseCase(params: mobilePhone).then((userUid) {
+    await _firebaseAuthRepository
+        .submitPhoneNumber(mobilePhone)
+        .then((userUid) {
       emit(MobilePhoneVerified(userUid));
       this.userUid = userUid;
     }).catchError((e) {
       emit(CubitAuthFailed(e.toString()));
     });
   }
+
   Future<void> submitOTP(String otpCode) async {
     emit(OTPSubmitting());
-    await _submitOtpPhoneUseCase(params: otpCode).then((userUid) {
+    await _firebaseAuthRepository.submitOTP(otpCode).then((userUid) {
       emit(OTPSubmitted(userUid));
       this.userUid = userUid;
     }).catchError((e) {
       emit(CubitAuthFailed(e.toString()));
     });
   }
+
   Future<void> logOut(String mobilePhone) async {
     emit(AuthLoggingOut());
-    await _logOutUseCase(params: null).then((_) {
+    await _firebaseAuthRepository.logOut().then((_) {
       emit(AuthLoggedOut());
     }).catchError((e) {
       emit(CubitAuthFailed(e.toString()));
