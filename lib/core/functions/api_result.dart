ff10c15a9f5c8480aed63ecf23e3ae3308a78758
ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-01-04 23:03:02 +0200

    create ApiResult

---
+++ lib/core/functions/api_result.dart
@@ -0,0 +1,12 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:uber/core/functions/network_exceptions.dart';
+
+part 'api_result.freezed.dart';
+
+@freezed
+ class ApiResult<T> with _$ApiResult<T> {
+  const factory ApiResult.success(T data) = Success<T>;
+
+  const factory ApiResult.failure(NetworkExceptions networkExceptions) =
+      Failure<T>;
+}
