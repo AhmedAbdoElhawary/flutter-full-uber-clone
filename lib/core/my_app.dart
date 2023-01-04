07d2eacc7130efeb40fe3faed6369f85e1a0e022
ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-01-04 23:09:21 +0200

    refactoring file without use cases

--- lib/core/my_app.dart
+++ lib/core/my_app.dart
@@ -19,10 +19,6 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  @override
-  void initState() {
-    super.initState();
-  }
 
   @override
   Widget build(BuildContext context) {
