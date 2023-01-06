7e8a581a27ff51606d6b0cbdc31aae7f7205fefb
ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-01-06 02:17:24 +0200

    create PlaceLocationInfo

---
+++ lib/data/models/place_location_info/place_location_info.dart
@@ -0,0 +1,66 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'place_location_info.g.dart';
+
+@JsonSerializable(includeIfNull: true)
+class PlaceLocationInfo {
+  Result? result;
+  String? status;
+
+  PlaceLocationInfo({this.result, this.status});
+
+  factory PlaceLocationInfo.fromJson(Map<String, dynamic> json) =>
+      _$PlaceLocationInfoFromJson(json);
+
+  Map<String, dynamic> toJson() => _$PlaceLocationInfoToJson(this);
+}
+
+@JsonSerializable(includeIfNull: true)
+class Result {
+  Geometry? geometry;
+
+  Result({this.geometry});
+
+  factory Result.fromJson(Map<String, dynamic> json) => _$ResultFromJson(json);
+
+  Map<String, dynamic> toJson() => _$ResultToJson(this);
+}
+
+@JsonSerializable(includeIfNull: true)
+class Geometry {
+  Location? location;
+  Viewport? viewport;
+
+  Geometry({this.location, this.viewport});
+
+  factory Geometry.fromJson(Map<String, dynamic> json) =>
+      _$GeometryFromJson(json);
+
+  Map<String, dynamic> toJson() => _$GeometryToJson(this);
+}
+
+@JsonSerializable(includeIfNull: true)
+class Location {
+  double? lat;
+  double? lng;
+
+  Location({this.lat, this.lng});
+
+  factory Location.fromJson(Map<String, dynamic> json) =>
+      _$LocationFromJson(json);
+
+  Map<String, dynamic> toJson() => _$LocationToJson(this);
+}
+
+@JsonSerializable(includeIfNull: true)
+class Viewport {
+  Location? northeast;
+  Location? southwest;
+
+  Viewport({this.northeast, this.southwest});
+
+  factory Viewport.fromJson(Map<String, dynamic> json) =>
+      _$ViewportFromJson(json);
+
+  Map<String, dynamic> toJson() => _$ViewportToJson(this);
+}
