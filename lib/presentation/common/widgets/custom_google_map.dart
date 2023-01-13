0c4dd05577938ddf4d7695f67d2beea3eb0c36d5
ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-01-13 02:06:37 +0200

    rebuild CustomGoogleMap for get location

--- lib/presentation/common/widgets/custom_google_map.dart
+++ lib/presentation/common/widgets/custom_google_map.dart
@@ -5,41 +5,55 @@ import 'package:uber/core/resources/color_manager.dart';
 import 'package:uber/presentation/pages/map/logic/map_logic.dart';
 
 class CustomGoogleMap extends StatelessWidget {
-  const CustomGoogleMap({required this.mapControl, super.key});
-  final MapLogic mapControl;
+  const CustomGoogleMap({this.tag="small map tag", this.id="small map id", super.key});
+  final String tag;
+  final String id;
   @override
   Widget build(BuildContext context) {
-    return Obx(
-      () {
-        if (mapControl.getCurrentPosition != null) {
-          return Listener(
-            onPointerDown: (PointerDownEvent details) {
-              if (!mapControl.showLocationIcon.value) {
-                mapControl.changeShowingIcon(true);
-              }
-            },
-            child: GoogleMap(
-              mapType: MapType.normal,
-              myLocationEnabled: true,
-              zoomControlsEnabled: false,
-              myLocationButtonEnabled: false,
-              initialCameraPosition:
-                  mapControl.getMyCurrentLocationCameraPosition,
-              onMapCreated: (GoogleMapController controller) {
-                try {
-                  mapControl.mapController.value.complete(controller);
-                } catch (e) {
-                  NullThrownError;
+    return GetBuilder<MapLogic>(
+      id: id,
+      tag: tag,
+      initState: (state) {
+        if (tag=="small map tag") {
+          Get.put<MapLogic>(MapLogic(), tag: tag);
+        } else {
+          Get.find<MapLogic>(tag: tag);
+        }
+      },
+      builder: (mapControl) {
+        switch (mapControl.getCurrentPosition) {
+          case null:
+            return Container(
+              width: double.infinity,
+              height: double.infinity,
+              color: ColorManager.lowOpacityGrey,
+            );
+          default:
+            return Listener(
+              onPointerDown: (PointerDownEvent details) {
+                if (!mapControl.showLocationIcon.value) {
+                  mapControl.changeShowingIcon(true);
                 }
               },
-            ),
-          );
-        } else {
-          return Container(
-            width: double.infinity,
-            height: double.infinity,
-            color: ColorManager.lowOpacityGrey,
-          );
+              child: GoogleMap(
+
+                markers: mapControl.travelMarkers.value,
+                mapType: MapType.normal,
+                myLocationEnabled: true,
+                zoomControlsEnabled: false,
+                myLocationButtonEnabled: false,
+
+                initialCameraPosition:
+                    mapControl.getMyCurrentLocationCameraPosition,
+                onMapCreated: (GoogleMapController controller) {
+                  try {
+                    mapControl.mapController.value.complete(controller);
+                  } catch (e) {
+                    NullThrownError;
+                  }
+                },
+              ),
+            );
         }
       },
     );
