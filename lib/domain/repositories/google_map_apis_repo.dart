8c0455d3482a6aca44acd907aca9619f14edbb2b
ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-01-13 02:08:17 +0200

    refactoring the code

--- lib/domain/repositories/google_map_apis_repo.dart
+++ lib/domain/repositories/google_map_apis_repo.dart
@@ -3,7 +3,8 @@ import 'package:uber/data/models/place_suggestion/places_suggestions.dart';
 import 'package:uber/data/models/place_location_info/place_location_info.dart';
 
 abstract class GoogleMapAPIsRepo {
-  Future<ApiResult<PlacesSuggestions>> getPlacesSuggestions({required String input});
-  Future<ApiResult<PlaceLocationInfo>> getPlacesLocation({required String placeId});
-
+  Future<ApiResult<PlacesSuggestions>> getPlacesSuggestions(
+      {required String input});
+  Future<ApiResult<List<PlaceLocationInfo>>> getPlacesLocation(
+      {required List<String> placesIds});
 }
