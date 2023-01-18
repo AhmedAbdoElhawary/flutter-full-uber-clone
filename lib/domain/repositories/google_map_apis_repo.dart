32c6240cede3205bb0cefba1afc105b76c130b98
ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-01-18 19:51:16 +0200

    add getPlacesDirection in GoogleMapAPIsRepo

--- lib/domain/repositories/google_map_apis_repo.dart
+++ lib/domain/repositories/google_map_apis_repo.dart
@@ -1,10 +1,13 @@
 import 'package:uber/core/functions/api_result.dart';
 import 'package:uber/data/models/place_suggestion/places_suggestions.dart';
 import 'package:uber/data/models/place_location_info/place_location_info.dart';
+import 'package:uber/data/models/places_direction/places_direction.dart';
 
 abstract class GoogleMapAPIsRepo {
   Future<ApiResult<PlacesSuggestions>> getPlacesSuggestions(
       {required String input});
   Future<ApiResult<List<PlaceLocationInfo>>> getPlacesLocation(
       {required List<Prediction> places});
+  Future<ApiResult<PlacesDirection>> getPlacesDirection(
+      {required String startPoint,required String endPoint});
 }
