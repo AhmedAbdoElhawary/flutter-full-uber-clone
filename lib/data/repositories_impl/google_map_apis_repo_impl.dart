6681b72a16465bd33fea63a43a0c94f4b4327ff0
ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-01-18 19:51:08 +0200

    add getPlacesDirection in GoogleMapAPIsRepoImpl

--- lib/data/repositories_impl/google_map_apis_repo_impl.dart
+++ lib/data/repositories_impl/google_map_apis_repo_impl.dart
@@ -3,6 +3,7 @@ import 'package:uber/core/functions/network_exceptions.dart';
 import 'package:uber/data/data_sources/remote/api/google_map_apis.dart';
 import 'package:uber/data/models/place_suggestion/places_suggestions.dart';
 import 'package:uber/data/models/place_location_info/place_location_info.dart';
+import 'package:uber/data/models/places_direction/places_direction.dart';
 import 'package:uber/domain/repositories/google_map_apis_repo.dart';
 import 'package:uuid/uuid.dart';
 
@@ -38,6 +39,18 @@ class GoogleMapAPIsRepoImpl implements GoogleMapAPIsRepo {
       }
 
       return ApiResult.success(result);
+    } catch (e) {
+      return ApiResult.failure(NetworkExceptions.getDioException(e));
+    }
+  }
+
+  @override
+  Future<ApiResult<PlacesDirection>> getPlacesDirection(
+      {required String startPoint, required String endPoint}) async {
+    try {
+      PlacesDirection data = await _googleMapAPIs.getPlacesDirection(
+          origin: startPoint, destination: endPoint);
+      return ApiResult.success(data);
     } catch (e) {
       return ApiResult.failure(NetworkExceptions.getDioException(e));
     }
