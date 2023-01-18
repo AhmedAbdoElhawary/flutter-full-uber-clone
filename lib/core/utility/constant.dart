32f55c478dd10a5981fb1a2f18a3ebdd6bed830b
ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-01-18 18:14:32 +0200

    change the name of some variables

--- lib/core/utility/constant.dart
+++ lib/core/utility/constant.dart
@@ -2,5 +2,4 @@ import 'package:flutter/material.dart';
 
 String personalId = "";
 Size sizeOfScreen = const Size(0, 0);
-const baseUrl =
-    'https://maps.googleapis.com/maps/api/place/';
+const baseUrl = 'https://maps.googleapis.com/maps/api/place/';
