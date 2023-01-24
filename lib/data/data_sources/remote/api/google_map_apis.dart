5792c22abdffcd865886eb439d0ff4107156a806
ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-01-25 01:30:18 +0200

    delete getPlacesDirection from GoogleMapAPIs

--- lib/data/data_sources/remote/api/google_map_apis.dart
+++ lib/data/data_sources/remote/api/google_map_apis.dart
@@ -4,7 +4,6 @@ import 'package:uber/core/utility/constant.dart';
 import 'package:uber/core/utility/private_keys.dart';
 import 'package:uber/data/models/place_location_info/place_location_info.dart';
 import 'package:uber/data/models/place_suggestion/places_suggestions.dart';
-import 'package:uber/data/models/places_direction/places_direction.dart';
 part 'google_map_apis.g.dart';
 
 @RestApi(baseUrl: baseUrl)
@@ -24,13 +23,6 @@ abstract class GoogleMapAPIs {
     @Query("place_id") required String placeId,
     @Query("key") final String apiKey = mapApiKey,
     @Query("sessiontoken") required String sessionToken,
-  });
-
-  @GET("directions/json")
-  Future<PlacesDirection> getPlacesDirection({
-    @Query("key") final String apiKey = mapApiKey,
-    @Query("origin") required String origin,
-    @Query("destination") required String destination,
   });
 
 }
