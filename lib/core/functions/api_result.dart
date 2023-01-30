c1e5c09e484a3da48fc99174482a0ad6f0a6adb0
ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-01-30 20:22:18 +0200

    format the code

--- lib/core/functions/api_result.dart
+++ lib/core/functions/api_result.dart
@@ -4,7 +4,7 @@ import 'package:uber/core/functions/network_exceptions.dart';
 part 'api_result.freezed.dart';
 
 @freezed
- class ApiResult<T> with _$ApiResult<T> {
+class ApiResult<T> with _$ApiResult<T> {
   const factory ApiResult.success(T data) = Success<T>;
 
   const factory ApiResult.failure(NetworkExceptions networkExceptions) =
